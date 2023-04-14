e71566d381b76553d6aa72452ef3bdcca9b35bc9
SullivanENPC <sullivan.castro@eleves.enpc.fr>
2023-04-14 14:50:27 +0200

    add comments

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ import joblib
 app = Flask(__name__)
 
 
-# Main function here
-# ------------------
+############################################# HOME PAGE #############################################
 @app.route('/', methods=['GET', 'POST'])
 def predict():
+    """
+    The predict function receives the information through the form. Then the model makes a prediction
+    and the pages is updated.
+    """
     # If a form is submitted
     if request.method == "POST":
 
         # Unpickle classifier
-        clf = joblib.load("models/model.pkl")
+        model = joblib.load("models/model.pkl")
 
         # Get values through input bars
         SL = request.form.get("SepalLength")
@@ -25,48 +28,51 @@ def predict():
         PW = request.form.get("PetalWidth")
 
         # Get prediction
-        prediction = int_to_flower[clf.predict([[SL, SW, PL, PW]])[0]]
+        prediction = int_to_flower[model.predict([[SL, SW, PL, PW]])[0]]
 
     else:
         prediction = ""
 
     return render_template("index.html", output=prediction)
 
+
+############################################# DATABASE PAGE #############################################
 @app.route('/database.html', methods=['GET', 'POST'])
 def add_to_database():
-    # If a form is submitted
-    df = pd.read_pickle("models/database.pkl")
+    # Load the database
+    database = pd.read_pickle("models/database.pkl")
 
     if request.method == "POST":
         try:
-            # Unpickle classifier
-            clf = joblib.load("models/model.pkl")
+            # Get values through input bars and initiate the new element
+            new_element = {
+                'SL': request.form.get("SepalLength"),
+                'SW': request.form.get("SepalWidth"),
+                'PL': request.form.get("PetalLength"),
+                'PW': request.form.get("PetalWidth"),
+                'FN': request.form.get("FlowerName")
+            }
 
-            # Get values through input bars
-            SL = request.form.get("SepalLength")
-            SW = request.form.get("SepalWidth")
-            PL = request.form.get("PetalLength")
-            PW = request.form.get("PetalWidth")
-            FN = request.form.get("FlowerName")
+            # Create a DataFrame with the same column than the database
+            database = database.append(new_element, ignore_index=True)
 
-            # Get prediction
-            df = pd.read_pickle("models/database.pkl")
-            X = pd.DataFrame([[SL, SW, PL, PW, FN]], columns=df.columns)
-            df = pd.concat([df, X])
+            # Save the database
+            database.to_pickle("models/database.pkl")
 
-            df.to_pickle("models/database.pkl")
-            accuracy = fit_model(df, "models/model.pkl")
+            # Train the model and measure the new performance
+            accuracy = fit_model(database, "models/model.pkl")
             prediction = "Added to the database"
 
         except Exception as e:
             print(f"The error {e} occured.")
             prediction = "An error occured"
-            accuracy = fit_model(df, "models/model.pkl")
+            accuracy = fit_model(database, "models/model.pkl")
 
     else:
         prediction = "You haven't modified the dataset already."
-        accuracy = fit_model(df, "models/model.pkl")
+        accuracy = fit_model(database, "models/model.pkl")
 
+    # Update the page
     return render_template("database.html", output=prediction, accuracy=accuracy)
 
 # Running the app
--- models/model.py
+++ models/model.py
@@ -9,31 +9,32 @@ int_to_flower = {'0': 'setosa', '1': 'versicolor', '2': 'virginica'}
 flower_to_int = dict(zip(int_to_flower.values(), int_to_flower.keys()))
 
 
-def fit_model(df, path):
-    # Sélectionner les features et la target
-    X = df.iloc[:, :-1]
-    y = df.iloc[:, -1].replace(flower_to_int)
+def fit_model(database, path):
+    """
+    Train the model and update the weights
+    """
+    # Select features and targets
+    X = database.iloc[:, :-1]
+    y = database.iloc[:, -1].replace(flower_to_int)
 
-    # Diviser les données en ensembles d'entraînement et de test
+    # Divide the data in train and test dataset
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    # Initialiser un modèle SVM avec un noyau linéaire
+    # Load the model
     model = SVC(kernel='linear', C=1)
 
-    # Entraîner le modèle sur l'ensemble d'entraînement
+    # Train the model
     model.fit(X_train, y_train)
 
-    # Prédire les classes pour l'ensemble de test
+    # Measure the performance on the test set
     y_pred = model.predict(X_test)
-
-    # Calculer la précision (accuracy) du modèle
     accuracy = accuracy_score(y_test, y_pred)
 
+    # Save the model
     joblib.dump(model, path)
 
     return accuracy
 
 if __name__ == "__main__":
-    # Charger les données depuis l'URL
-    df = pd.read_pickle("./database.pkl")
-    print(f"Précision du modèle : {fit_model(df, 'model.pkl')}")
+    database = pd.read_pickle("./database.pkl")
+    print(f"Précision du modèle : {fit_model(database, 'model.pkl')}")
